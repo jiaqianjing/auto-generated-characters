f06fb909d8b39a2fe10208d8a6900393707943c8
jiaqianjing <jiaqianjing@gmail.com>
2022-01-14 15:50:29 +0800

    update

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # auto-generated-characters
-面部表情/动作迁移 + 唇形同步
+## Quick Start
+1. 假设用户已经熟悉 docker 并且已经正确安装 docker，执行以下步骤，开箱即用(浏览器输入：http://localhost:8899)
+    ```
+    # 1. pull image & run container
+    ./start_dev.sh
+    # 2. 进入 container 运行时环境
+    docker exec -it dev bash
+    # 3. 启动 notebook
+    ./start_jupyterlab.sh
+    ```
+2. 在浏览器中打开 `jupyterlab`
+http://localhost:8899
--- start_dev.sh
+++ start_dev.sh
@@ -1,9 +1,11 @@
-image='auto-gen-characters:1.0.0'
+image='neurons/auto-gen-characters:1.0.0'
 container_name='dev'
 cmd='sleep 1000d'
+# [Tips]: host 模式在非 linux 下，可能不支持，因此启动容器时，同时开了一个用于宿主机访问容器内 jupyterlab 的端口 8899
 nvidia-docker run  -itd --name $container_name  \
                    --restart=always \
                    --network host \
+                   -p 8899:8899 \
                    -v $PWD:/workspace \
                    $image  \
                    $cmd
